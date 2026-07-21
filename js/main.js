f4a8fe975c326272e4e70ae682ada3200c333281
Ezequiel Senosiain <ezesenosiain@gmail.com>
2026-07-21 15:59:48 -0300

    pre-entrega 5

--- js/main.js
+++ js/main.js
@@ -1,45 +1,70 @@
-let saldo = 1000;
-let opcion = "";
+class CuentaBancaria {
 
-let movimientos = [
-    "Saldo inicial",
-    "Inicio de sesión",
-    "Consulta de saldo",
-    "Depósito",
-    "Retiro"
-];
+    constructor(titular, numeroCuenta, saldo, movimientos) {
+        this.titular = titular;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.movimientos = movimientos;
+    }
 
-function mostrarSaldo(saldoActual) {
-    alert("Su saldo es: $" + saldoActual);
-}
+    depositar(monto) {
+        this.saldo += monto;
+    }
 
-function actualizarSaldo(saldoActual, monto, operacion) {
-
-    if (operacion === "deposito") {
-        return saldoActual + monto;
-    } else {
-        return saldoActual - monto;
+    retirar(monto) {
+        if (monto <= this.saldo) {
+            this.saldo -= monto;
+            return true;
+        }
+        return false;
     }
 
 }
 
-const agregarMovimiento = (movimiento) => {
-    movimientos.push(movimiento);
+const cuenta1 = new CuentaBancaria(
+    "Juan",
+    1001,
+    1000,
+    ["Saldo inicial"]
+);
+
+const cuenta2 = new CuentaBancaria(
+    "María",
+    1002,
+    2500,
+    ["Saldo inicial"]
+);
+
+const cuenta3 = new CuentaBancaria(
+    "Pedro",
+    1003,
+    500,
+    ["Saldo inicial"]
+);
+
+let opcion = "";
+
+function mostrarSaldo(cuenta) {
+    alert("Su saldo es: $" + cuenta.saldo);
+}
+
+const agregarMovimiento = (cuenta, movimiento) => {
+    cuenta.movimientos.push(movimiento);
 };
 
-function mostrarHistorial() {
+function mostrarHistorial(cuenta) {
 
     let buscar = prompt(
         "Ingrese un movimiento para buscar.\nEjemplo: Consulta de saldo"
     );
 
-    if (movimientos.includes(buscar)) {
+    if (cuenta.movimientos.includes(buscar)) {
 
-        let posicion = movimientos.indexOf(buscar);
+        let posicion = cuenta.movimientos.indexOf(buscar);
 
         alert("El movimiento existe y está en la posición " + posicion);
 
-        movimientos.splice(posicion, 1, buscar + " (revisado)");
+        cuenta.movimientos.splice(posicion, 1, buscar + " (revisado)");
 
     } else {
 
@@ -47,14 +72,15 @@ function mostrarHistorial() {
 
     }
 
-    let eliminado = movimientos.pop();
+    let eliminado = cuenta.movimientos.pop();
+
     alert("Se eliminó el último movimiento: " + eliminado);
 
-    movimientos.unshift("Historial actualizado");
+    cuenta.movimientos.unshift("Historial actualizado");
 
     console.log("Historial de movimientos");
 
-    for (let movimiento of movimientos) {
+    for (let movimiento of cuenta.movimientos) {
         console.log(movimiento);
     }
 
@@ -75,16 +101,15 @@ while (opcion !== "5") {
 
     if (opcion === "1") {
 
-        mostrarSaldo(saldo);
-        agregarMovimiento("Consulta de saldo");
+        mostrarSaldo(cuenta1);
+        agregarMovimiento(cuenta1, "Consulta de saldo");
 
     } else if (opcion === "2") {
 
         let deposito = Number(prompt("Ingrese el monto a depositar"));
 
-        saldo = actualizarSaldo(saldo, deposito, "deposito");
-
-        agregarMovimiento("Depósito de $" + deposito);
+        cuenta1.depositar(deposito);
+        agregarMovimiento(cuenta1, "Depósito de $" + deposito);
 
         alert("Depósito realizado.");
 
@@ -92,11 +117,9 @@ while (opcion !== "5") {
 
         let retiro = Number(prompt("Ingrese el monto a retirar"));
 
-        if (retiro <= saldo) {
+        if (cuenta1.retirar(retiro)) {
 
-            saldo = actualizarSaldo(saldo, retiro, "retiro");
-
-            agregarMovimiento("Retiro de $" + retiro);
+            agregarMovimiento(cuenta1, "Retiro de $" + retiro);
 
             alert("Retiro realizado.");
 
@@ -108,7 +131,7 @@ while (opcion !== "5") {
 
     } else if (opcion === "4") {
 
-        mostrarHistorial();
+        mostrarHistorial(cuenta1);
 
     } else if (opcion === "5") {
 
